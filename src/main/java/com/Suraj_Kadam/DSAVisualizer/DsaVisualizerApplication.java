0b1b3d7c25747364a5190de21ab8863ed65abd11
Suraj Kadam <kadamsuraj4567890@gmail.com>
2025-10-06 12:22:20 +0530

    Initial commit: Project setup for DSA Visualizer

---
+++ src/main/java/com/Suraj_Kadam/DSAVisualizer/DsaVisualizerApplication.java
@@ -0,0 +1,13 @@
+package com.Suraj_Kadam.DSAVisualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaVisualizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaVisualizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Suraj_Kadam/DSAVisualizer/DsaVisualizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Suraj_Kadam.DSAVisualizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaVisualizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
